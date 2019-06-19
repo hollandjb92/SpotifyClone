5117f8034f692765a1113086d022cb8f0bebcea7
hollandjb <hollandjb@vcu.edu>
2019-06-19 19:11:25 -0400

    added access token to fetch

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
 
           fetch("https://api.spotify.com/v1/me", {
             headers: {
-              "Authorization": "Bearer ${payload}"
+              "Authorization": "Bearer " + token
             }
           }).then(response => {
             return response.json()
